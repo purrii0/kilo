7c85939e6b194b5b2f0de65ecd0117c3423a69a5
purrii0 <sabinpuri0815@gmail.com>
2026-04-07 18:27:27 +0545

    raw i/p

--- kilo.c
+++ kilo.c
@@ -13,10 +13,15 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-  ARROW_LEFT = 'a',
-  ARROW_RIGHT = 'd',
-  ARROW_UP = 'w',
-  ARROW_DOWN = 's'
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -56,7 +61,7 @@ void enableRawMode() {
     die("tcsetattr");
 }
 
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
@@ -65,27 +70,60 @@ char editorReadKey() {
   }
   if (c == '\x1b') {
     char seq[3];
-
     if (read(STDIN_FILENO, &seq[0], 1) != 1)
       return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1)
       return '\x1b';
-
     if (seq[0] == '[') {
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '3':
+            return DEL_KEY;
+          case '4':
+            return END_KEY;
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
+        }
+      }
+    } else if (seq[0] == 'O') {
       switch (seq[1]) {
-      case 'A':
-        return ARROW_UP;
-      case 'B':
-        return ARROW_DOWN;
-      case 'C':
-        return ARROW_RIGHT;
-      case 'D':
-        return ARROW_LEFT;
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
       }
     }
     return '\x1b';
+  } else {
+    return c;
   }
-  return c;
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -190,25 +228,29 @@ void editorRefreshScreen() {
 }
 
 /*** input ***/
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
   case ARROW_LEFT:
-    E.cx--;
+    if (E.cx != 0)
+      E.cx--;
     break;
   case ARROW_RIGHT:
-    E.cx++;
+    if (E.cx != E.screenCols - 1)
+      E.cx++;
     break;
   case ARROW_UP:
-    E.cy--;
+    if (E.cy != 0)
+      E.cy--;
     break;
   case ARROW_DOWN:
-    E.cy++;
+    if (E.cy != E.screenRows - 1)
+      E.cy++;
     break;
   }
 }
 
 void editorProcessKeyPress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
 
   switch (c) {
   case CTRL_KEY('q'):
@@ -216,6 +258,18 @@ void editorProcessKeyPress() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+  case END_KEY:
+    E.cx = E.screenCols - 1;
+    break;
+  case PAGE_UP:
+  case PAGE_DOWN: {
+    int times = E.screenRows;
+    while (times--)
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+  } break;
   case ARROW_LEFT:
   case ARROW_DOWN:
   case ARROW_UP:
